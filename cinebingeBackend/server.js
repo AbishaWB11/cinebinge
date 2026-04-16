f7578be686bce90e914b46b17e999bac0810ff1a
Abisha Winslet <abishawinslet1101@gmail.com>
2026-04-16 22:30:27 +0530

    fixed mongo connection

--- cinebingeBackend/server.js
+++ cinebingeBackend/server.js
@@ -21,6 +21,6 @@ app.get("/", (req, res) => {
     res.send("Welcome to CineBinge API");
 });
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running");
 });
